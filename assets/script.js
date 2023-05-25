ce0b282582e17dcaee98b0f1a146fa7a4daa4a23
HannahMidd <hmiddleton1017@gmail.com>
2023-05-25 07:44:01 -0400

    adding PW requirement updates

--- assets/script.js
+++ assets/script.js
@@ -92,7 +92,10 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 function passwordRequirements() {
   var length = parseInt(
-    prompt("How many characters does your password need to have? (Between 8-128)"),10
+    prompt(
+      "How many characters (between 8-128) would you like your password to have?"
+    ),
+    10
   );
   
   // Controlling the length of the password (must be between 8-128 characters)
@@ -105,45 +108,50 @@ function passwordRequirements() {
     alert("Please enter a number between 8-128");
     return null;
 
-      // If user does not enter a numerical value, this error message will occur. 
-  if (Number.isNaN(length)) {
-    alert('Password length must be provided as a number');
-    return null;
-  }
-   
-  // Variables that will be used for user requirements for: special characters, numbers, uppercase and lowercase letters. 
+    // If user does not enter a numerical value, this error message will occur.
+    if (Number.isNaN(length)) {
+      alert("Password length must be provided as a number");
+      return null;
+    }
+
+    // Variables that will be used for user requirements for: special characters, numbers, uppercase and lowercase letters.
 
     var hasSpecialCharacters = confirm(
-    'Click OK to confirm including special characters.'
-  );
+      "Click OK to confirm including special characters."
+    );
 
-  var hasNumbers = confirm(
-    'Click OK to confirm including numbers.'
-  );
+    var hasNumbers = confirm("Click OK to confirm including numbers.");
 
-  
-  var hasLowerCaseLetters = confirm(
-    'Click OK to confirm including lowercase letters.'
-  );
+    var hasLowerCaseLetters = confirm(
+      "Click OK to confirm including lowercase letters."
+    );
 
-  var hasUpperCaseLetters = confirm(
-    'Click OK to confirm including uppercase letters.'
-  );
+    var hasUpperCaseLetters = confirm(
+      "Click OK to confirm including uppercase letters."
+    );
 
-// Confirming that user has to use at least one of the special characters, numbers, lower/uppercase letters. (Must come after var)
+    // Confirming that user has to use at least one of the special characters, numbers, lower/uppercase letters. (Must come after var)
 
- if (
-   hasSpecialCharacters === false &&
-   hasNumericCharacters === false &&
-   hasLowerCasedCharacters === false &&
-   hasUpperCasedCharacters === false
- ) {
-   alert("Please select at least one character type");
-   return null;
- }
+    if (
+      hasSpecialCharacters === false &&
+      hasNumericCharacters === false &&
+      hasLowerCasedCharacters === false &&
+      hasUpperCasedCharacters === false
+    ) {
+      alert("Please select at least one character type");
+      return null;
+    }
 
+    // Object to store user input
+    var passwordRequirements = {
+      length: length,
+      hasSpecialCharacters: hasSpecialCharacters,
+      hasNumbers: hasNumbers,
+      hasLowerCaseLetters: hasLowerCaseLetters
+      hasUpperCaseLetters: hasUpperCaseLetters,
+    };
 
-
+    return passwordRequirements;
   }
     
     
