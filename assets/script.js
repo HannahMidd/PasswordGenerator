323eecfdda2c25662017cf8853b1ba77d2cb9ac8
HannahMidd <hmiddleton1017@gmail.com>
2023-05-24 22:03:00 -0400

    var added to JS

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+//Special Characters to be used in PW Generator
 var specialCharacters = [
   "{",
   "]",
@@ -24,6 +26,69 @@ var specialCharacters = [
   "?",
   ":",
 ];
+
+//Uppercase Letters to be used in PW Generator
+var upperCaseLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+//Lowercase Letters to be used in PW Generator
+var lowerCaseLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+//Numbers to be used in PW Generator
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Write password to the #password input
 function writePassword() {
